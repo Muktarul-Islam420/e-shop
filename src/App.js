de5b9eefc0086f9379be50173b20f5ec20ec7adc
khan4200000 <www.muktarul420@gmail.com>
2021-09-19 04:57:57 +0600

    nav bar product cart some feature done one product added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react'
+//import Products from './components/Products/Products'
+import {Products, Navbar} from './components'
+import {commerce} from './lib/commerce';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [products, setProducts] = useState([]);
+
+
+    const fetchProducts = async () => {
+        const {data} = await commerce.products.list()
+    
+        setProducts(data);
+    }
+
+    useEffect(() => {
+        fetchProducts();
+    })
+    console.log(products)
+    
+    return (
+        <div>
+            <Products/>
+            <Navbar/>
+        </div>
+    )
 }
 
-export default App;
+export default App
